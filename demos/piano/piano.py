652b7dea0b43cad1f7bbdec4956a6f390c305438
Miriam Ruiz <miriam@debian.org>
2019-04-02 15:22:23 +0200

    Make the piano play some music

--- demos/piano/piano.py
+++ demos/piano/piano.py
@@ -4,7 +4,11 @@
 import os
 import sys
 import json
+import time
+import random
 import pyglet
+import fluidsynth
+from threading import Thread
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(this_dir, '..', '..'))
@@ -50,7 +54,68 @@ if fast:
     OpenGL.ERROR_ON_COPY = True
     OpenGL.STORE_POINTERS = False
 
+class MidiSoundPlayer():
+    def __init__(self, keyboard_handler=None):
+        self.keyboard_handler = keyboard_handler
+        self.fs = fluidsynth.Synth()
+        self.fs.start(driver="alsa")
+        print("FluidSynth Started")
+        self.sfid = self.fs.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
+        self.fs.program_select(0, self.sfid, 0, 0)
+    def __del__(self): # See:https://eli.thegreenplace.net/2009/06/12/safely-using-destructors-in-python/
+        self.fs.delete()
+        print("FluidSynth Closed")
+        del self.fs
+    def press(self, key, velocity=64, duration=0.5):
+        self.fs.noteon(0, key + 19, velocity)
+        if self.keyboard_handler: self.keyboard_handler.press(key + 19, True)
+        time.sleep(duration)
+        self.fs.noteoff(0, key + 19)
+        if self.keyboard_handler: self.keyboard_handler.press(key + 19, False)
+    @staticmethod
+    def random_key(mean_key=44):
+        x = random.gauss(mean_key, 10.0)
+        if x < 1: x = 1
+        elif x > 88: x = 88
+        return int(round(x))
+    @staticmethod
+    def random_velocity():
+        x = random.gauss(100.0, 10.0)
+        if x < 1: x = 1
+        elif x > 127: x = 127
+        return int(round(x))
+    @staticmethod
+    def random_duration(self, mean_duration=2.0):
+        x = random.gauss(mean_duration, 2.0)
+        if x < 0.2: x = 0.2
+        return x
+    def random_play(self, num, mean_key, mean_duration):
+        while num != 0:
+            num -= 1
+            key = self.random_key(mean_key)
+            velocity = self.random_velocity()
+            duration = self.random_duration(mean_duration)
+            self.press(key, velocity, duration)
+
+def adj_color(red, green, blue, factor):
+    return (int(red*factor), int(green*factor), int(blue*factor))
+
 class MusicKeybOctave():
+    NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+    # See: https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+    COLORS_RGB = [
+        (230, 25, 75),   (60, 180, 75),   (255, 225, 25),  (67, 99, 216),   (245, 130, 49),
+        (145, 30, 180),  (66, 212, 244),  (240, 50, 230),  (191, 239, 69),  (250, 190, 190),
+        (70, 153, 144),  (230, 190, 255), (154, 99, 36),   (255, 250, 200), (128, 0, 0),
+        (170, 255, 195), (128, 128, 0),   (255, 216, 177), (0, 0, 117),     (169, 169, 169),
+    ]
+
+    COLORS = [
+        [sg.Color(*adj_color(r, g, b, 1.25)) for (r, g, b) in COLORS_RGB], # White keys
+        [sg.Color(*adj_color(r, g, b, 1.0/1.25)) for (r, g, b) in COLORS_RGB], # Black keys
+    ]
+
     def __init__(self):
         with open(os.path.join(this_dir, "PianoKeyboard.svg")) as f:
             svg = f.read()
@@ -60,16 +125,21 @@ class MusicKeybOctave():
         json.dump(self.model_root, sys.stdout, cls=JSONDebugEncoder, indent=2, sort_keys=True)
         #json.dump(self.model_elements, sys.stdout, cls=JSONDebugEncoder, indent=2, sort_keys=True)
         #sys.stdout.write("\n") # Python JSON dump misses last newline
+        self.orig_fill_color = {}
+        for note in self.NOTES:
+            self.orig_fill_color[note] = self.model_elements[note].fill
     def root(self):
         return self.model_root
     def size(self):
         (x_min, y_min), (x_max, y_max) = self.model_root.aabbox()
         return (x_max - x_min), (y_max - y_min)
     def press(self, key, action=True):
-        self.model_elements[key].fill = sg.Color.red
+        if action:
+            self.model_elements[key].fill = self.COLORS[0 if len(key) == 1 else 1][10]
+        else:
+            self.model_elements[key].fill = self.orig_fill_color[key]
 
 class MusicKeyboard():
-    NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
     def __init__(self, num_octaves=10):
         self.octaves = [MusicKeybOctave() for i in range(0, num_octaves)]
         self.elements = []
@@ -89,13 +159,17 @@ class MusicKeyboard():
         return self.model_root
     def press(self, num_key, action=True):
         num_octave = num_key // 12
-        piano.octaves[num_octave].press(self.NOTES[num_key % 12], action)
+        piano.octaves[num_octave].press(MusicKeybOctave.NOTES[num_key % 12], action)
 
 piano = MusicKeyboard()
 scene = piano.root()
 window_size = int(piano.width + 2 * margin), int(piano.height + 2 * margin)
 
 feedback = sg.Group(fill=None, stroke=sg.Color.red)
+
+midi_player = MidiSoundPlayer(piano)
+midi_thread = Thread(target = midi_player.random_play, args = (8, 10, 0.3))
+midi_thread.start()
 
 #piano.press(3)
 
@@ -162,3 +236,6 @@ def update(dt, window=None):
 pyglet.clock.schedule_interval(update, 1/60, window)
 
 pyglet.app.run()
+
+midi_thread.join()
+print("All threads finished")
